cc87dc5af35018641957b98f5a93585393076290
Anuj857 <anujkryadav02042005@gmail.com>
2026-07-14 14:51:26 +0530

    Fix Lambda archive source directory

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -2,7 +2,7 @@ data "archive_file" "lambda_zip" {
 
   type = "zip"
 
-  source_dir = "${path.module}/../build"
+  source_dir = "${path.module}/../src"
 
   output_path = "${path.module}/lambda.zip"
 
